84e0fd6be207cd253df1b046f5e33b07bcc694a2
Dmitri Rysak <dimarusak5@gmail.com>
2025-01-29 17:52:27 +0300

    error_2

--- src/test/java/api/tests/dima/utils/SeleniumUtiles.java
+++ src/test/java/api/tests/dima/utils/SeleniumUtiles.java
@@ -3,9 +3,10 @@ package api.tests.dima.utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Ignore;
 
 import java.time.Duration;
-
+@Ignore
 public class SeleniumUtiles {
 
     private static WebDriver driver = new ChromeDriver();
